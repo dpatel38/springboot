5a0f4eaf7d189da4e23e0e7eb35964188d06561f
dpatel38 <daminipatels@gmail.com>
2021-05-14 08:13:49 -0400

    Create a project using spring boot.

---
+++ src/main/java/com/tts/springboot/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.tts.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories("com.tts.springboot.repository")
+@EntityScan("com.tts.springboot.model")
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tts/springboot/controller/exception/BookNotFoundException.java
@@ -0,0 +1,16 @@
+package com.tts.springboot.controller.exception;
+
+public class BookNotFoundException extends RuntimeException{
+
+    public BookNotFoundException(){
+        super();
+    }
+
+    public BookNotFoundException(final String message, final Throwable cause){
+        super(message, cause);
+    }
+
+    public BookNotFoundException(final Throwable cause){
+        super(cause);
+    }
+}
